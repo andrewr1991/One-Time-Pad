7b5a6713298d7d1bdcca18eb1b0df3b090042fee
Andrew Ridout <Fridout106@gmail.com>
2017-05-14 00:57:02 -0500

    Partial functionality

--- onetimepad.cpp
+++ onetimepad.cpp
@@ -7,18 +7,18 @@
 
 using namespace std;
 
-int encrypt() {
+void encrypt() {
 	srand(time(NULL));
   /*string file;
   cout << "Enter a file name: ";
   cin >> file;*/
 
-  ifstream ist{"text.txt"};
+  ifstream textIst{"text.txt"};
 	ofstream encryptedOst{"encrypted.txt"};
 	ofstream keyOst{"key.txt"};
 	char c;
 
-	while (ist.get(c)) {
+	while (textIst.get(c)) {
 		int C;
 		int r = rand() % 53 + 1;
 		if ((c + r) > 126) {
@@ -28,13 +28,35 @@ int encrypt() {
 		else {
 			C = c + r;
 		}
-		keyOst << C << "\n";
+		keyOst << r << "\n";
 		encryptedOst << (char)C;
 		cout << c << " " << (int)c << " " << r << " " << C << endl;
 	}
 }
 
+void decrypt() {
+	ifstream keyIst{"key.txt"};
+	ifstream encryptedIst{"encrypted.txt"};
+	ofstream decryptedOst{"decrypted.txt"};
+	string key;
+	char c;
+	int k;
+
+	while (getline(keyIst, key) && encryptedIst.get(c)) {
+		k = stoi(key);
+		cout << c << " " << k << endl;
+		if (c < 100) {
+			c = 126 - (k - (c - 32));
+		}
+		else {
+			c = (c - k);
+		}
+		decryptedOst << c;
+	}
+}
+
 int main() {
 	encrypt();
+	decrypt();
   return 0;
 }
